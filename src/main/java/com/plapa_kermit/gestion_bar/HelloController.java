f94337c8e9b40d265529ac3b7ae1307d12b77ec4
PlapaKermit <92321646+PlapaKermit@users.noreply.github.com>
2023-05-09 21:36:20 +0200

    Initial Commit

--- src/main/java/com/plapa_kermit/gestion_bar/HelloController.java
+++ src/main/java/com/plapa_kermit/gestion_bar/HelloController.java
@@ -2,6 +2,7 @@ package com.plapa_kermit.gestion_bar;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+//DOIT QUE modifier
 //Rajout une fonction qui affiche qqchose sur l'ecran
 public class HelloController {
     @FXML //Rentre la vue interactive
