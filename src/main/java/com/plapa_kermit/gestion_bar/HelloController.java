3f47f4c7224bfc811243364d13e18253b5fe6c15
PlapaKermit <92321646+PlapaKermit@users.noreply.github.com>
2023-05-11 16:11:13 +0200

    Def du projet

--- src/main/java/com/plapa_kermit/gestion_bar/HelloController.java
+++ src/main/java/com/plapa_kermit/gestion_bar/HelloController.java
@@ -3,14 +3,18 @@ package com.plapa_kermit.gestion_bar;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 //Si possible trier dans des packages Vue, Model , Controller
 
 //DOIT QUE modifier
 //Rajout une fonction qui affiche qqchose sur l'ecran
 public class HelloController {
+    @FXML
+    public TextField nameField;
     @FXML //Rentre la vue interactive
     private Label welcomeText;
+    @FXML private TextField tittel;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -19,8 +23,11 @@ public class HelloController {
 
     @FXML
     protected void onTestButtonClick(ActionEvent actionEvent) {
-        welcomeText.setText("TEST");
+        welcomeText.setText("Test button clicked!");
     }
 
-
+    @FXML
+    protected void handleButton1Action(ActionEvent actionEvent) {
+        System.out.println(nameField.getText());
+    }
 }
